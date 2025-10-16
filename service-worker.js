7172f6d026993c4a079427b7d99f6176b38e96ba
David Angarita <davidangarita.sistemas@gmail.com>
2025-10-16 17:25:38 -0500

    Update shared-files

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,22 @@
 const CACHE_NAME = "quinejo-v1";
-const urlsToCache = ["index.html", "share.html"];
+const urlsToCache = ["/", "/index.html", "/share.html", "/assets/icon.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((names) =>
+        Promise.all(names.map((n) => n !== CACHE_NAME && caches.delete(n)))
+      )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
